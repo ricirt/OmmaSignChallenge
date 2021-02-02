be17eb5fa168ca4d5ed518fb046a4494d7021ac5
fatih <fatih@DESKTOP-PT432LC>
2021-02-02 18:39:06 +0300

    second stucture created

--- Operation.js
+++ Operation.js
@@ -88,3 +88,103 @@ function firstStructure() {
   animate();
   setInterval(animate, 4000);
 }
+
+
+
+function secondStructure() {
+  if (document.body.contains(document.querySelector(".wrapper"))) {
+    document.querySelector(".wrapper").remove();
+  }
+  var wrapper = document.createElement("div");
+  wrapper.className = "wrapper";
+  document.body.appendChild(wrapper);
+  wrapper.style.backgroundImage = "url('/images/bg.jpg')";
+  //first desset Dom
+  var horizontal = document.createElement("div");
+  horizontal.className = "horizontal";
+
+  var firstDessert = document.createElement("div");
+  firstDessert.className = "first-dessert";
+
+  var dessertFirstHeader = document.createElement("div");
+  dessertFirstHeader.className = "dessert-header";
+  var dessertFirstHeaderText = document.createElement("h1");
+  dessertFirstHeader.appendChild(dessertFirstHeaderText);
+  firstDessert.appendChild(dessertFirstHeader);
+
+  var dessertImage = document.createElement("div");
+  dessertImage.className = "dessert-image";
+  var firstImage = document.createElement("img");
+  dessertImage.appendChild(firstImage);
+  firstDessert.appendChild(dessertImage);
+
+  var dessertFirstPrice = document.createElement("div");
+  dessertFirstPrice.className = "dessert-price";
+  var dessertPriceText = document.createElement("h1");
+  dessertFirstPrice.appendChild(dessertPriceText);
+  firstDessert.appendChild(dessertFirstPrice);
+
+  //second dessert Dom
+  var secondDessert = document.createElement("div");
+  secondDessert.className = "second-dessert";
+
+  var dessertSecondHeader = document.createElement("div");
+  dessertSecondHeader.className = "dessert-header";
+  var dessertSecondHeaderText = document.createElement("h1");
+  dessertSecondHeader.appendChild(dessertSecondHeaderText);
+  secondDessert.appendChild(dessertSecondHeader);
+
+  var dessertSecondPrice = document.createElement("div");
+  dessertSecondPrice.className = "dessert-price";
+  var dessertSecondPriceText = document.createElement("h1");
+  dessertSecondPrice.appendChild(dessertSecondPriceText);
+  secondDessert.appendChild(dessertSecondPrice);
+
+  var dessertSecondImage = document.createElement("div");
+  dessertSecondImage.className = "dessert-image";
+  var secondImage = document.createElement("img");
+  dessertSecondImage.appendChild(secondImage);
+  secondDessert.appendChild(dessertSecondImage);
+
+  horizontal.appendChild(secondDessert);
+  //third dessert dom
+
+  var thirdDessert = document.createElement("div");
+  thirdDessert.className = "third-dessert";
+
+  var dessertThirdHeader = document.createElement("div");
+  dessertThirdHeader.className = "dessert-header";
+  var dessertThirdHeaderText = document.createElement("h1");
+  dessertThirdHeader.appendChild(dessertThirdHeaderText);
+  thirdDessert.appendChild(dessertThirdHeader);
+
+  var dessertThirdPrice = document.createElement("div");
+  dessertThirdPrice.className = "dessert-price";
+  var dessertThirdPriceText = document.createElement("h1");
+  dessertThirdPrice.appendChild(dessertThirdPriceText);
+  thirdDessert.appendChild(dessertThirdPrice);
+
+  var dessertThirdImage = document.createElement("div");
+  dessertThirdImage.className = "dessert-image";
+  var thirdImage = document.createElement("img");
+  dessertThirdImage.appendChild(thirdImage);
+  thirdDessert.appendChild(dessertThirdImage);
+
+  horizontal.appendChild(firstDessert);
+  horizontal.appendChild(secondDessert);
+  horizontal.appendChild(thirdDessert);
+
+  dessertFirstHeader.innerHTML = list[3].productName;
+  dessertFirstPrice.innerHTML = list[3].productPrice;
+  firstImage.src = list[3].productImage;
+
+  dessertSecondHeader.innerHTML = list[4].productName;
+  dessertSecondPrice.innerHTML = list[4].productPrice;
+  secondImage.src = list[4].productImage;
+
+  dessertThirdHeader.innerHTML = list[5].productName;
+  dessertThirdPrice.innerHTML = list[5].productPrice;
+  thirdImage.src = list[5].productImage;
+
+  document.querySelector(".wrapper").appendChild(horizontal);
+}
